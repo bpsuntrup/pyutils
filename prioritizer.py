51e4253ae989a1e9aed547c0f331545ad31a935c
Benjamin Suntrup <bpsuntrup@outlook.com>
2018-07-26 00:04:34 -0500

    librarify prioritizer

--- prioritizer.py
+++ prioritizer.py
@@ -1,5 +1,13 @@
 ''' prioritizer.py
-takes a list of tasks and aids in prioritization of tasks. '''
+takes a list of tasks and aids in prioritization of tasks.
+
+TODO: test
+TODO: extend so that priorities are saved in database
+TODO: add gui
+TODO: add api for easily adding tasks
+TODO: add ability to remove tasks found uninteresting
+TODO: add ability to back up database, or even to version control it
+'''
 import itertools
 from bite import bite
 
@@ -18,7 +26,6 @@ tasks = [
     'develop an app',
 ]
 
-tasks = { task: 0 for task in tasks}
 
 def best(tasks):
     """ tasks must be a dict of tasks and non-negative integers """
@@ -34,42 +41,47 @@ def best(tasks):
             bests[task] = val
     return bests
 
-dangling_task = None
+def prioritize(tasks):
+    tasks = { task: 0 for task in tasks}
+    dangling_task = None
 
-
-
-while len(best(tasks).items()) > 1:
-    i = iter(best(tasks))
-    if dangling_task is not None:
-        i = itertools.chain(i, [dangling_task,])
-        dangling_task = None
-    for task1, task2 in bite(i):
-        if task2 is None:
-            dangling_task = task1
-            break
-
-        # Now get user input
-        print('1. {}'.format(task1))
-        print('2. {}'.format(task2))
-        print('3. Quit and exit.')
-        while True:
-            try:
-                answer = int(input())
-                if answer not in [1,2,3]:
-                    raise ValueError
+    while len(best(tasks).items()) > 1:
+        i = iter(best(tasks))
+        if dangling_task is not None:
+            i = itertools.chain(i, [dangling_task,])
+            dangling_task = None
+        for task1, task2 in bite(i):
+            if task2 is None:
+                dangling_task = task1
                 break
-            except ValueError:
-                print('Input must be 1, 2, or 3.')
-        if answer == 1:
-            print('you chose: {}'.format(task1))
-            tasks[task1] = max(tasks[task2] + 1, tasks[task1])
-        elif answer == 2:
-            print('you chose: {}'.format(task2))
-            tasks[task2] = max(tasks[task1] + 1, tasks[task2])
-        else:
-            print best((tasks))
-            exit(0)
 
-best_task = best(tasks)
-print ( 'Results: {}'.format(best_task))
-print ('You should \"{}\"'.format(best_task.keys()[0]))
+            # Now get user input
+            print('')
+            print('Would you rather: ')
+            print('1. {}'.format(task1))
+            print('2. {}'.format(task2))
+            print('3. Quit and exit.')
+            while True:
+                try:
+                    answer = int(input('> '))
+                    if answer not in [1,2,3]:
+                        raise ValueError
+                    break
+                except ValueError:
+                    print('Input must be 1, 2, or 3.')
+            if answer == 1:
+                print('You chose: \"{}\"'.format(task1))
+                tasks[task1] = max(tasks[task2] + 1, tasks[task1])
+            elif answer == 2:
+                print('you chose: {}'.format(task2))
+                tasks[task2] = max(tasks[task1] + 1, tasks[task2])
+            else:
+                print best((tasks))
+                exit(0)
+
+    best_task = best(tasks)
+    print ( 'Results: {}'.format(best_task))
+    print ('You should \"{}\"'.format(best_task.keys()[0]))
+
+if __name__ == '__main__':
+    prioritize(tasks)
